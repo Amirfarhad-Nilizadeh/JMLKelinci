01698115628a8b6e4815581700437772a4e45bff
Gary T. Leavens <leavens@eecs.ucf.edu>
2021-03-12 17:41:12 -0500

    Update instrument.sh
    Use shell environment variables.

--- ShellScripts/instrument.sh
+++ ShellScripts/instrument.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-cd src
-javac -cp ".:/home/~/kelinci-master/instrumentor/build/libs/kelinci.jar" *.java -d ../bin
+#!/bin/sh
+: ${Kel=~/kelinci-master} 
+: ${OJ=~/tools/openjml}
+cd jml
+java -jar "$OJ/openjml.jar" -rac -racPreconditionEntry *.java
 cd ..
-java -jar /home/~/kelinci-master/instrumentor/build/libs/kelinci.jar -i ./bin -o ./bin-instr
+cd src
+javac -cp ".:$Kel/instrumentor/build/libs/kelinci.jar" *.java -d ../bin
+cd ..
+java -jar "$Kel/instrumentor/build/libs/kelinci.jar" -i ./bin -o ./bin-instr
 java -cp ./bin-instr/ edu.cmu.sv.kelinci.Kelinci KelinciDriverMain @@
 
