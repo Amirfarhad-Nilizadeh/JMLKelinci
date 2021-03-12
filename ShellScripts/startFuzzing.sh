9a6841a3dbb0ec42a76a8b7bca8442b9870e63e8
Gary T. Leavens <leavens@eecs.ucf.edu>
2021-03-12 17:22:02 -0500

    Update startFuzzing.sh
    Syntax fix for sh.

--- ShellScripts/startFuzzing.sh
+++ ShellScripts/startFuzzing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 : ${Kel=/~/kelinci-master}  
-AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 "$Kel/afl-2.52b/afl-fuzz" -i in_dir -o fuzzer-out -S afl -t 999999999 "$Kel/fuzzerside/interface" @@
+AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 "$Kel/afl-2.52b/afl-fuzz" -i in_dir -o fuzzer-out -S afl -t 999999999 "$Kel/fuzzerside/interface" "$@"
