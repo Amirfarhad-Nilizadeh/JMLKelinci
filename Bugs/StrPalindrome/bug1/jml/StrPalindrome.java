080cbec653f32fc7e7368fb3223c0fd0445b21fe
Amirfarhad-Nilizadeh <amirfarhad.nilizadeh@gmail.com>
2021-03-14 03:46:35 -0400

    Update

--- Bugs/StrPalindrome/bug1/jml/StrPalindrome.java
+++ Bugs/StrPalindrome/bug1/jml/StrPalindrome.java
@@ -1,9 +1,8 @@
 public class StrPalindrome {
-   private /*@ spec_public @*/ String reverse = "";
    /*@ public normal_behavior
      @    requires (\forall int i; 0 <= i && i < str.length(); (int)Character.MIN_VALUE <= (int)str.charAt(i) && (int)str.charAt(i) <= (int)Character.MAX_VALUE);
- @*/
-   public boolean isPalindrome(String str) {   
+   @*/
+   public boolean isPalindrome(String str) {
       return true;
    }
 } 
